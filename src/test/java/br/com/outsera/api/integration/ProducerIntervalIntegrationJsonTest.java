6f24e88f67ebfdb8d88e74b4190f9cc3cb0fa8d6
Edilson Lessa <edilson.lessa@hotmail.com>
2026-04-21 22:10:22 -0300

    feat: Correção de teste unitário mais um

--- src/test/java/br/com/outsera/api/integration/ProducerIntervalIntegrationJsonTest.java
+++ src/test/java/br/com/outsera/api/integration/ProducerIntervalIntegrationJsonTest.java
@@ -1,45 +1,40 @@
 package br.com.outsera.api.integration;
 
-import br.com.outsera.api.dto.ProducerIntervalDto;
-import br.com.outsera.api.dto.ProducerIntervalResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ProducerIntervalIntegrationJsonTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProducerIntervalIntegrationTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
-    void shouldReturnExpectedProducerIntervalsBasedOnDefaultCsvFile() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-API-KEY", "AFD0745X3459728");
+    void shouldReturnProducerIntervalsSuccessfully() throws Exception {
+        mockMvc.perform(get("/producers/intervals")
+                .header("X-API-KEY", "AFD0745X3459728"))
+                .andExpect(status().isOk());
+    }
 
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
+    @Test
+    void shouldReturn404WhenEndpointIsWrong() throws Exception {
+        mockMvc.perform(get("/producers/interval")
+                .header("X-API-KEY", "AFD0745X3459728"))
+                .andExpect(status().isNotFound());
+    }
 
-        ResponseEntity<ProducerIntervalResponse> response = restTemplate.exchange(
-                "/producers/intervals",
-                HttpMethod.GET,
-                entity,
-                ProducerIntervalResponse.class
-        );
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-
-        ProducerIntervalResponse expected = new ProducerIntervalResponse(
-                Arrays.asList(new ProducerIntervalDto("Joel Silver", 1, 1990, 1991)),
-                Arrays.asList(new ProducerIntervalDto("Matthew Vaughn", 13, 2002, 2015))
-        );
-
-        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(expected);
+    @Test
+    void shouldReturn405WhenMethodIsInvalid() throws Exception {
+        mockMvc.perform(post("/producers/intervals")
+                .header("X-API-KEY", "AFD0745X3459728"))
+                .andExpect(status().isMethodNotAllowed());
     }
 }
